2da7195c0c2e8795007270dbc712e5aee17fe0b5
ParkTaeHo98 <154481176+ParkTaeHo98@users.noreply.github.com>
2023-12-27 15:03:16 +0900

    속도

--- Assets/Scripts/LoadingSceneManager.cs
+++ Assets/Scripts/LoadingSceneManager.cs
@@ -45,7 +45,7 @@ public class LoadingSceneManager : MonoBehaviour
                 progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
                 if (progressBar.fillAmount == 1.0f)
                 {
-                    yield return new WaitForSeconds(3.0f);
+                    yield return new WaitForSeconds(0.0f);
                     op.allowSceneActivation = true;
                     break;
                 }
