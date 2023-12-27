6072d2291f8b041bbd830ab7413585ba5848e2ab
ParkTaeHo98 <154481176+ParkTaeHo98@users.noreply.github.com>
2023-12-27 18:10:01 +0900

    수정본

--- Assets/Scripts/ending.cs
+++ Assets/Scripts/ending.cs
@@ -11,32 +11,19 @@ public class ending : MonoBehaviour
 
     void Start()
     {
-        Invoke("StartEding", 2.0f);
-        Debug.Log("시작");
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (ok)
-        {
             if (credits.transform.position.y >= 9700.0f)
             {
                 SceneManager.LoadScene("StageScene");
-                Debug.Log("스크룰");
             }
             else
             {
                 credits.transform.position += new Vector3(0, speed, 0);
-                Debug.Log("끝");
             }
-        }
-    }
-
-    void StartEding()
-    {
-        ok = true; ; 
-        Debug.Log("함수");
     }
     public void skips()
     {
